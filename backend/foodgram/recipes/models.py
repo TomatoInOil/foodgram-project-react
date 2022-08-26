24996255154fde72126e0860c80a3c61ed05b8ec
Daniil Pautov <danpautoff@yandex.ru>
2022-08-26 15:27:19 +0300

    Добавление модели ингредиентов и связи с рецептами.

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Ingredient(models.Model):
+    """Модель ингредиентов."""
+
+    name = models.CharField(max_length=200, verbose_name="Название")
+    measurement_unit = models.CharField(
+        max_length=16, verbose_name="Единицы измерения"
+    )
+
+
 class Recipe(models.Model):
     """Модель рецептов."""
 
@@ -13,9 +22,24 @@ class Recipe(models.Model):
         User, on_delete=models.CASCADE, verbose_name="Автор"
     )
     ingredients = models.ManyToManyField(
-        "Ingredients", verbose_name="Ингредиенты"
+        Ingredient,
+        through="IngredientQuantity",
+        through_fields=("recipe", "ingredient"),
+        verbose_name="Ингредиенты",
     )
     image = models.ImageField(verbose_name="Картинка")
     text = models.TextField(verbose_name="Текстовое описание")
     cooking_time = models.IntegerField(verbose_name="Время приготовления")
     pub_date = models.DateTimeField(auto_now_add=True, db_index=True)
+
+
+class IngredientQuantity(models.Model):
+    """Связь рецепта и ингредиента с указанием количества."""
+
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, verbose_name="Рецепт"
+    )
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, verbose_name="Ингредиент"
+    )
+    number = models.FloatField(verbose_name="Количество")
