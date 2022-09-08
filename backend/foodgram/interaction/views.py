e9b0725985a56f5dd61de4f771e9d7c88b8f3c35
Даниил Паутов <danpautoff@yandex.ru>
2022-09-08 12:27:49 +0300

    Оптимизация представлений приложения interaction.

--- backend/foodgram/interaction/views.py
+++ backend/foodgram/interaction/views.py
@@ -1,18 +1,16 @@
-from pathlib import Path
-
-from django.conf import settings
 from django.http import HttpResponse
 from rest_framework import status, views
 from rest_framework.response import Response
 
 from interaction.models import FavoriteRecipesList, ShoppingList
 from interaction.serializers import RecipeSerializer
-from interaction.shortcuts import (
-    create_error_response,
-    get_or_create_obj_owned_by_user,
-    get_recipes_from_model_on_request,
+from interaction.sevices import (
+    add_selected_recipe_to_recipes_list,
+    delete_selected_recipe_from_recipes_list,
+    get_filepath_related_to_shopping_list,
+    get_or_create_obj_owned_by_current_user,
     get_selected_recipe_on_request,
-    get_year,
+    write_text_of_shopping_list_to_file,
 )
 
 
@@ -29,19 +27,14 @@ class RecipesListView(views.APIView):
 
     def post(self, request, *args, **kwargs):
         """Добавляет рецепт в список избранных рецептов пользователя."""
-        recipes_from_list = get_recipes_from_model_on_request(
-            self.model, request, *args, **kwargs
-        )
         selected_recipe = get_selected_recipe_on_request(
             request, *args, **kwargs
         )
-
-        if selected_recipe in recipes_from_list.all():
-            return create_error_response(
-                msg=f"Рецепт уже добавлен в список {self.name_of_list}."
-            )
-
-        recipes_from_list.add(selected_recipe)
+        error_response = add_selected_recipe_to_recipes_list(
+            selected_recipe, request, self.model, self.name_of_list
+        )
+        if error_response:
+            return error_response
         return Response(
             data=RecipeSerializer(instance=selected_recipe).data,
             status=status.HTTP_201_CREATED,
@@ -49,19 +42,14 @@ class RecipesListView(views.APIView):
 
     def delete(self, request, *args, **kwargs):
         """Удаляет связь рецепта и списка избранных рецептов пользователя."""
-        recipes_from_list = get_recipes_from_model_on_request(
-            self.model, request, *args, **kwargs
-        )
         selected_recipe = get_selected_recipe_on_request(
             request, *args, **kwargs
         )
-
-        if selected_recipe not in recipes_from_list.all():
-            return create_error_response(
-                msg=f"Рецепта нет в списке {self.name_of_list}."
-            )
-
-        recipes_from_list.remove(selected_recipe)
+        error_response = delete_selected_recipe_from_recipes_list(
+            selected_recipe, request, self.model, self.name_of_list
+        )
+        if error_response:
+            return error_response
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
@@ -86,43 +74,10 @@ class DownloadShoppingListView(views.APIView):
 
     def get(self, request, *args, **kwargs):
         """Отдаёт файл со списком покупок пользователя."""
-        current_user = request.user
-        shopping_list = get_or_create_obj_owned_by_user(
-            current_user, self.model
+        shopping_list = get_or_create_obj_owned_by_current_user(
+            request, self.model
         )
-
-        shopping_lists_path = Path.joinpath(
-            settings.MEDIA_ROOT, "shopping_lists/"
-        )
-        if not settings.MEDIA_ROOT.exists():
-            Path.mkdir(settings.MEDIA_ROOT)
-        if not shopping_lists_path.exists():
-            Path.mkdir(shopping_lists_path)
-        filepath = Path.joinpath(
-            shopping_lists_path,
-            f"shopping_list_{shopping_list.id}.txt",
-        )
-
-        with open(filepath, "w", encoding="utf-8") as file:
-            rows = []
-
-            for recipe in shopping_list.recipes.all():
-                rows.append(f"- - - {recipe.name} - - - \n")
-                recipe_ingredients = recipe.ingredients.all()
-                for ingredient in recipe_ingredients:
-                    name = ingredient.ingredient.name
-                    amount = str(ingredient.amount)
-                    measurement_unit = ingredient.ingredient.measurement_unit
-                    rows.append(
-                        " ".join(["[ ]", name, amount, measurement_unit, "\n"])
-                    )
-
-            host = request.get_host()
-            year = get_year()
-            rows.append(f"{host} {year}")
-
-            file.writelines(rows)
-            file.close()
-
+        filepath = get_filepath_related_to_shopping_list(shopping_list)
+        write_text_of_shopping_list_to_file(filepath, shopping_list, request)
         with open(filepath, "r", encoding="utf-8") as file:
             return HttpResponse(file, status=status.HTTP_200_OK)
