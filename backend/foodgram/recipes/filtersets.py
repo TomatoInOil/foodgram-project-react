f394653c077379944adb72c65a83f795c53feafd
Даниил Паутов <danpautoff@yandex.ru>
2022-09-13 19:30:57 +0300

    Фильтрация рецептов по спискам рецептов и авторам.

--- backend/foodgram/recipes/filtersets.py
+++ backend/foodgram/recipes/filtersets.py
@@ -4,10 +4,24 @@ from recipes.models import Recipe
 
 
 class RecipeFilter(filters.FilterSet):
-    """Фильтр рецептов по тегам с помощью slug."""
+    """Фильтр рецептов по тегам, спискам рецептов, авторам."""
 
     tags = filters.CharFilter(field_name="tags__slug", lookup_expr="iexact")
+    is_favorited = filters.BooleanFilter(
+        field_name="favoriterecipeslists__user",
+        method="filter_is_in_recipes_list",
+    )
+    is_in_shopping_cart = filters.BooleanFilter(
+        field_name="shoppinglists__user", method="filter_is_in_recipes_list"
+    )
+
+    def filter_is_in_recipes_list(self, queryset, name, value):
+        current_user = self.request.user
+        lookup = "__".join([name, "exact"])
+        if value:
+            return queryset.filter(**{lookup: current_user})
+        return queryset.exclude(**{lookup: current_user})
 
     class Meta:
         model = Recipe
-        fields = ["tags"]
+        fields = ["author"]
